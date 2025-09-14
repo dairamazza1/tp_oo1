5c74330137a5319d3536c0dedb24b90209f95219
Daira Mazza <daira0796@hotmail.com>
2025-09-14 12:50:44 -0300

    Puntos 1, 2 y 3 de la guía

--- src/Torneo/Torneo.java
+++ src/Torneo/Torneo.java
@@ -1,71 +1,173 @@
 package Torneo;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Torneo {
 	private int id;
 	private String nombre;
 	private String temporada;
-	private int asistencias;
-	private Jugador jugador;
-	private Partido partido;
+	private List<Equipo> listaEquipos;
+	private LocalDate fechInicio;
+	private LocalDate fechaFinal;
 	
-	public Torneo(String nombre, String temporada, int asistencias, Jugador jugador, Partido partido) {
+	
+	
+	public Torneo(int id, String nombre, String temporada, LocalDate fechInicio, LocalDate fechaFinal) {
+		this.id = id;
 		this.nombre = nombre;
 		this.temporada = temporada;
-		this.asistencias = asistencias;
-		this.jugador = jugador;
-		this.partido = partido;
-	
+		this.fechInicio = fechInicio;
+		this.fechaFinal = fechaFinal;
+		
+		this.listaEquipos = new ArrayList<Equipo>();
 	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	public String getTemporada() {
-		return temporada;
-	}
-	public int getAsistencias() {
-		return asistencias;
-	}
-	public Jugador getJugador() {
-		return jugador;
-	}
-	public Partido getPartido() {
-		return partido;
-	}
-	
+
+
+
 	public int getId() {
 		return id;
 	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public String getTemporada() {
+		return temporada;
+	}
+
+
+
+	public List<Equipo> getEquipo() {
+		return listaEquipos;
+	}
+
+
+
+	public LocalDate getFechInicio() {
+		return fechInicio;
+	}
+
+
+
+	public LocalDate getFechaFinal() {
+		return fechaFinal;
+	}
+
+
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
+
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
+
+
 	public void setTemporada(String temporada) {
 		this.temporada = temporada;
 	}
-	public void setAsistencias(int asistencias) {
-		this.asistencias = asistencias;
+
+
+
+
+
+	public void setFechInicio(LocalDate fechInicio) {
+		this.fechInicio = fechInicio;
 	}
-	public void setJugador(Jugador jugador) {
-		this.jugador = jugador;
+
+
+
+	public void setFechaFinal(LocalDate fechaFinal) {
+		this.fechaFinal = fechaFinal;
 	}
-	public void setPartido(Partido partido) {
-		this.partido = partido;
-	}
+
+	
 
 	@Override
 	public String toString() {
-		return "Toneo [nombre=" + nombre + ", temporada=" + temporada + ", asistencias=" + asistencias + "]";
+		return "Torneo: "+nombre +" \nid=" + id + 
+				", \ntemporada=" + temporada + ", \nfechInicio=" + fechInicio + ", \nfechaFinal=" + fechaFinal + ", \n" + listaEquipos ;
 	}
-	
+
+
+
 	public boolean equals(Torneo torneo) {
 		return this.id == torneo.getId();
 	}
 	
 	
+	//Métodos CU
+	public Equipo traerEquipo(int codUnico) {
+		int i = 0;
+		boolean found = false;
+		Equipo e = null;
+		
+		while(i < listaEquipos.size() && !found) {
+			if(listaEquipos.get(i).getCodUnico() == codUnico) {
+				found = true;
+				e = listaEquipos.get(i);
+			}
+			i ++;
+		}
+		
+		return e;
+	}
+	
+	public boolean agregarEquipo(Equipo e) {
+		//pre cond: validar que el nombre no exista en la lista	
+		//post cond: agregar el equipo con un cod único
+		
+		int i = 0;
+		boolean found = false;
+		
+		while(i < listaEquipos.size() && !found) {
+			if(!found && listaEquipos.get(i).getNombre().equalsIgnoreCase(e.getNombre())) {
+				found = true;
+			}
+			i++;
+		}
+		
+		if(!found) {
+			int codUnico = 0;
+			if(listaEquipos.size() > 0) {
+				//agrego codUnico+1 en el útimo elemento de la lista
+				codUnico = listaEquipos.get(listaEquipos.size() - 1).getCodUnico() + 1;				
+			}
+			e.setCodUnico(codUnico);
+			
+			return listaEquipos.add(e);
+		}
+		//agregar excepcion
+		return false;
+	}
+	
+	public void modificarEquipo(int codUnico, String nombre) {
+		Equipo equipoFound = traerEquipo(codUnico);
+		
+		if(equipoFound != null) {
+			equipoFound.setNombre(nombre);
+		}
+	}
+	
+	public boolean eliminarEquipo(int codUnico) {
+		Equipo equipoFound = traerEquipo(codUnico);
+		
+		if(equipoFound != null) {
+			listaEquipos.remove(equipoFound);
+		}
+		//agegar excepción
+		return false;
+	}
 	
 }
